fc14cc4fa92a8d982d74f73ea6cdb6f5c4293726
Aryan Raj <134445776+Code18Aryan@users.noreply.github.com>
2025-07-11 22:07:57 +0530

    Update MYPRACTICEPATTERN3.java

--- MYPRACTICEPATTERN3.java
+++ MYPRACTICEPATTERN3.java
@@ -3,8 +3,7 @@ public class MYPRACTICEPATTERN3 {
     public static void main(String[] args) {
         int x, y, z;
 
-        for(x=1; x<=10; x++)
-        {
+        for(x=1; x<=10; x++){
             for(y=1; y<=x; y++)
             {
                 System.out.print("*");
